c716ff3731ef5f8880361604d63713c82d4a5f37
unknown <ericsshyus@gmail.com>
2023-02-21 13:36:33 -0500

    save work

--- src/pages/sign-in.page.tsx
+++ src/pages/sign-in.page.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
 const SignInPage = () => {
-  const URL = "http://localhost:5000/login/federated/facebook";
+  const URL = "http://localhost:3000/auth/login/federated/facebook";
   async function signIn() {
-    let response = await fetch(URL);
-    let data = response.json();
+    console.log("signIn");
+    // let response = await fetch(URL);
+    // let data = response.json()
+    window.open(URL, "_self");
+
+    //console.log(data);
   }
   return (
     <div>
       <button className="button facebook" onClick={signIn}>
         Sign in with Facebook
       </button>
+      <a
+        className="button facebook"
+        href="http://localhost:3000/auth/login/federated/facebook"
+      >
+        Sign in with Facebook
+      </a>
     </div>
   );
 };
